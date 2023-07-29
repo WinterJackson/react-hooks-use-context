60f75a529523fc7560629b623caed120f352d43f
WinterJackson <winterjacksonwj@gmail.com>
2023-07-29 15:25:12 +0300

    Completed

---
+++ src/context/user.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+// create the context
+const UserContext = React.createContext();
+
+// create a provider component
+function UserProvider({ children }) {
+    const currentUser = {
+        name: "Duane",
+        interests: ["Coding", "Biking", "Words ending in 'ing'"],
+        };
+        return (
+            <UserContext.Provider value={currentUser}> 
+                {children}
+            </UserContext.Provider>
+        );
+    }
+
+export { UserContext, UserProvider };
